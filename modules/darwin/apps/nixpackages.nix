5173dfc051d8c8f6d66c5f4e3dd837b58ba8364d
swaphb <s@swaphb.com>
2025-01-19 15:00:10 -0500

    move user specific apps to home manager.

--- modules/darwin/apps/nixpackages.nix
+++ modules/darwin/apps/nixpackages.nix
@@ -2,22 +2,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    _1password-cli
-    awscli
-    azure-cli
-    brave
-    discord
-    go
-    google-cloud-sdk
-    kubectl
-    krew
-    lens
-    slack
-    spotify
-    starship
-    teleport
-    tenv
-    vim
-    vscode
+
   ];
 }
--- modules/home/stephen/default.nix
+++ modules/home/stephen/default.nix
@@ -3,4 +3,26 @@
   imports = [
     ./terminal/default.nix
   ];
+
+  home = {
+    packages = with pkgs; [
+      _1password-cli
+      awscli
+      azure-cli
+      brave
+      discord
+      go
+      google-cloud-sdk
+      kubectl
+      krew
+      lens
+      slack
+      spotify
+      teleport
+      tenv
+      vim
+      vscode
+    ];
+
+  };
 }
