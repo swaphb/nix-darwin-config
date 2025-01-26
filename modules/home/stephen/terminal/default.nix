d0034c1e80cc7c746d6b05bfbf105b1820bd5445
swaphb <s@swaphb.com>
2025-01-26 16:45:19 -0500

    add cursor

--- modules/home/stephen/terminal/default.nix
+++ modules/home/stephen/terminal/default.nix
@@ -8,11 +8,13 @@
 
   # Some user-level dotfiles
   home.file = {
+    # Configure ssh
     ".ssh/config".text = ''
       Host *
         IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
     '';
 
+    # Configure git
     ".gitconfig".text = ''
       [user]
         name = swaphb
@@ -28,11 +30,34 @@
       [commit]
         gpgSign = true
     '';
+
+    # Configure 1password agent.toml
+    ".config/1Password/ssh/agent.toml".text = ''
+      # Examples can be found here:
+      #  https://developer.1password.com/docs/ssh/agent/config
+
+      [[ssh-keys]]
+      item = "github-auth-key"
+      vault = "Private"
+      
+      [[ssh-keys]]
+      item = "github-sign-key"
+      vault = "Private"
+      
+      [[ssh-keys]]
+      item = "Github-swaphb"
+      vault = "Private"
+      
+      [[ssh-keys]]
+      vault = "Employee"
+    '';
     
+    # Configure zsh
     ".zshrc".text = ''
       eval "$(starship init zsh)"
     '';
 
+    # Configure ghostty
     ".config/ghostty/config".text = ''
       background-opacity = 0.9
       theme = "dracula"
