b3ca4ad06150f94fc57b54d68a33c0b9277b8476
swaphb <s@swaphb.com>
2025-01-28 20:52:26 -0500

    fix: use correct path to home directory in dock configuration

--- modules/home/stephen/default.nix
+++ modules/home/stephen/default.nix
@@ -25,4 +25,24 @@
     ];
 
   };
+  system.defaults.dock = {
+    autohide = true;
+    orientation = "bottom";
+    persistent-apps = [
+      /Applications/Safari.app
+      /Applications/Ghostty.app
+      "/${pkgs.vscode}/Applications/Visual Studio Code.app" # Use the nixpkgs path to the app for apps installed via nix. This will automatically use the latest nix store path.
+      "/${pkgs.lens}/Applications/Lens.app"
+      "/${pkgs.slack}/Applications/Slack.app"
+      "/${pkgs.discord}/Applications/Discord.app"
+      "/${pkgs.spotify}/Applications/Spotify.app"
+      "/Applications/joplin.app"
+      # Add your persistent apps here
+    ];
+    persistent-others = [
+      "${config.home-manager.users.stephen.home.homeDirectory}/code"
+      "${config.home-manager.users.stephen.home.homeDirectory}/Downloads"
+      # Add your persistent others here
+    ];
+  };
 }
