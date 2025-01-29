44657d8b619635ed1536b59438951fa8eb10e35f
swaphb <s@swaphb.com>
2025-01-28 23:13:00 -0500

    moved dock back to appearance.nix

--- modules/darwin/system/appearance.nix
+++ modules/darwin/system/appearance.nix
@@ -1,6 +1,33 @@
-{ config, pkgs, lib, ... }:
-
+{ config, pkgs, lib, username, ... }:
+let
+  homeDirectory = "/Users/${username}";
+in
 {  
+  # If you also want to do e.g. Dock preferences from the same user-level file:
+  system.defaults.dock = {
+    autohide = true;
+    orientation = "bottom";
+    persistent-apps = [
+      /Applications/Safari.app
+      /Applications/Ghostty.app
+      "/Applications/Cursor.app"
+      "/${pkgs.vscode}/Applications/Visual Studio Code.app" # Use the nixpkgs path to the app for apps installed via nix. This will automatically use the latest nix store path.
+      "/${pkgs.lens}/Applications/Lens.app"
+      "/${pkgs.slack}/Applications/Slack.app"
+      "/${pkgs.discord}/Applications/Discord.app"
+      "/${pkgs.spotify}/Applications/Spotify.app"
+      "/Applications/joplin.app"
+      # Add your persistent apps here
+    ];
+    persistent-others = [
+      "${homeDirectory}/code"
+      "${homeDirectory}/Downloads"
+      # Add your persistent others here
+    ];
+    show-recents = false;
+    tilesize = 36; # Set the icon size on the dock; default is 64
+  };
+
   system.defaults.NSGlobalDomain = {
     AppleInterfaceStyle = "Dark";     # "Dark" or "Light" - Darkmode all the things
     # Add more NSGlobalDomain settings here
--- modules/home/stephen/default.nix
+++ modules/home/stephen/default.nix
@@ -24,28 +24,4 @@
       vscode
     ];
   };
-
-  targets.darwin.defaults = {
-    dock = {
-      autohide = true;
-      orientation = "bottom";
-      persistent-apps = [
-        "/Applications/Safari.app"
-        "/Applications/Ghostty.app"
-        "/Applications/Cursor.app"
-        "/${pkgs.vscode}/Applications/Visual Studio Code.app"
-        "/${pkgs.lens}/Applications/Lens.app"
-        "/${pkgs.slack}/Applications/Slack.app"
-        "/${pkgs.discord}/Applications/Discord.app"
-        "/${pkgs.spotify}/Applications/Spotify.app"
-        "/Applications/joplin.app"
-      ];
-      persistent-others = [
-        "${config.home.homeDirectory}/code"
-        "${config.home.homeDirectory}/Downloads"
-      ];
-      show-recents = false;
-      tilesize = 36;
-    };
-  };
 }
