575e9adfdd2cd25529d0fbc6a25bba0ec238c1f7
swaphb <s@swaphb.com>
2025-01-28 22:48:51 -0500

    moved docker settings to home manager

--- modules/home/stephen/default.nix
+++ modules/home/stephen/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, username, ... }:
+{ config, pkgs, lib, ... }:
 {
   imports = [
     ./terminal/default.nix
@@ -23,5 +23,29 @@
       vim
       vscode
     ];
+  };
+
+  targets.darwin.defaults = {
+    dock = {
+      autohide = true;
+      orientation = "bottom";
+      persistent-apps = [
+        "/Applications/Safari.app"
+        "/Applications/Ghostty.app"
+        "/Applications/Cursor.app"
+        "/${pkgs.vscode}/Applications/Visual Studio Code.app"
+        "/${pkgs.lens}/Applications/Lens.app"
+        "/${pkgs.slack}/Applications/Slack.app"
+        "/${pkgs.discord}/Applications/Discord.app"
+        "/${pkgs.spotify}/Applications/Spotify.app"
+        "/Applications/joplin.app"
+      ];
+      persistent-others = [
+        "${config.home.homeDirectory}/code"
+        "${config.home.homeDirectory}/Downloads"
+      ];
+      show-recents = false;
+      tilesize = 36;
+    };
   };
 }
